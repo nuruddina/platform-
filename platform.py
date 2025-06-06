fc48931c91e54c40d6d3f27491dd0e47c3bd99ff
nuruddina <nuruddina2547@gmail.com>
2025-06-06 14:18:23 +0700

    Update platform.py

--- platform.py
+++ platform.py
@@ -2,6 +2,22 @@ import streamlit as st
 from PIL import Image
 import os
 
-st.sidebar.success("option menu")
+def home_page()
+  st.write("this is home page")
+
+def pinworm_detail_page()
+  st.write("this is page detail")
+
+st.sidebar.image(image, use_container_width=True)
+st.sidebar.markdown("<div class='sidebar-title'>Platform Navigation</div>", unsafe_allow_html=True)
+    
+pages = {
+    "Home": home_page,
+    "Pinworm Detail": pinworm_detail_page,
+    }
+
+selected_page = st.sidebar.selectbox("Select a page", list(pages.keys()))
+pages[selected_page]()
+
 
 
