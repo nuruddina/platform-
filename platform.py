94c09f11d439c93c4cc7f4ba9c0475e58048885d
nuruddina <nuruddina2547@gmail.com>
2025-06-06 14:26:15 +0700

    Update platform.py

--- platform.py
+++ platform.py
@@ -17,7 +17,7 @@ st.sidebar.markdown("<div class='sidebar-title'>Platform Navigation</div>", unsa
 pages = {
     "Home": home_page,
     "Parasite egg Detail": parasite_detail_page,
-    "Ai DEtector": ai_detector_page,
+    "Ai Detector": ai_detector_page,
     }
 
 selected_page = st.sidebar.selectbox("Select a page", list(pages.keys()))
