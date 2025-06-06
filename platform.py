ce060bd4f4ac0bd95c0ca33594a0f0989f10d482
nuruddina <nuruddina2547@gmail.com>
2025-06-06 14:23:58 +0700

    Update platform.py

--- platform.py
+++ platform.py
@@ -5,14 +5,18 @@ import os
 def home_page():
   st.write("this is home page")
 
-def pinworm_detail_page():
+def parasite_detail_page():
   st.write("this is page detail")
+
+
+def ai_detector_page
 
 st.sidebar.markdown("<div class='sidebar-title'>Platform Navigation</div>", unsafe_allow_html=True)
     
 pages = {
     "Home": home_page,
-    "Pinworm Detail": pinworm_detail_page,
+    "Parasite egg Detail": parasite_detail_page,
+    "Ai DEtector": ai_detector_page,
     }
 
 selected_page = st.sidebar.selectbox("Select a page", list(pages.keys()))
