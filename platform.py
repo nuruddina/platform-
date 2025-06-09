6af2ef3e3c0a0ff32c2e23fddd118e4baf559596
nuruddina <nuruddina2547@gmail.com>
2025-06-09 17:46:45 +0700

    Update platform.py

--- platform.py
+++ platform.py
@@ -9,10 +9,26 @@ def home_page():
 
 def parasite_detail_page():
   st.write("this is page detail")
+  st.sidebar.title("Table of Content")
+  st.sidebar.markdown("""
+  -[Ascaris lumbricoides](#ascaris-lumbricoides)
+  -[Hymenolepsis nana](#hymenolepsis-nana)
+  -[Hymenolepsis diminuta}(#hymenolepsis-diminuta)
+  -[Tenia spp.](#tenia-spp.)
+  -[Trichuris trichiura](#trichuris-trichiura)
+  -[Hookworm](#hookworm)
+  -[Opisthorchis viverrini](#opisthorchis-viverrini)
+  -[Minute intestinal flukes](#minute-intestinal-flukes)
+""")
+ 
 
 
 def ai_detector_page():
   st.write("this is page ai detector")
+
+def quiz_page():
+  st.write("this is page quiz")
+
 
 st.sidebar.markdown("<div class='sidebar-title'>Platform Navigation</div>", unsafe_allow_html=True)
     
@@ -20,6 +36,7 @@ pages = {
     "Home": home_page,
     "Parasite egg Detail": parasite_detail_page,
     "Ai Detector": ai_detector_page,
+    "Quiz": quiz_page,
     }
 
 selected_page = st.sidebar.selectbox("Select a page", list(pages.keys()))
