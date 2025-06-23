4c9e0355887b9a6cdfd18a5ce360b1d777b0988c
nuruddina <nuruddina2547@gmail.com>
2025-06-23 18:17:55 +0700

    Update platform.py

--- platform.py
+++ platform.py
@@ -82,8 +82,9 @@ def parasite_detail_page():
   - **Unfertilized Eggs:** he unfertilized eggs are laid by uninseminated female. Thes are non-embryonated and cannot become infective.
   """)
   st.image('https://ars.els-cdn.com/content/image/3-s2.0-B9780128187319001075-f00107-14-9780128187319.jpg')
+  st.subheader("Adult")
   st.write("""
-  **Adult:** worms are characterized by their large size, cylindrical, unsegmented bodies, and distinct sexual dimorphism. Females are typically 20-35 cm long, while males are slightly smaller, ranging from 15-30 cm. Both sexes have three "lips" at the anterior end of the body. 
+  Adult worms are characterized by their large size, cylindrical, unsegmented bodies, and distinct sexual dimorphism. Females are typically 20-35 cm long, while males are slightly smaller, ranging from 15-30 cm. Both sexes have three "lips" at the anterior end of the body. 
   """)
   st.subheader("Life cycle")
   st.markdown("""
