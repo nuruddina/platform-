3c3026a27c94bf5be672079d7ae3892104c50ecd
nuruddina <nuruddina2547@gmail.com>
2025-06-27 21:10:52 +0700

    Update platform.py

--- platform.py
+++ platform.py
@@ -21,8 +21,8 @@ def home_page():
     with col2:
         st.subheader("Parasited image")
 
-        img_3 = Image.open("sample/TT_0661.tif")
-        img_4 = Image.open("sample/TT_2.png")
+        img_3 = Image.open("sample image/TT_0661.tif")
+        img_4 = Image.open("sample image/TT_2.png")
 
         st.image(img_3, caption="Input image", use_container_width=True)
         st.image(img_4, caption="Detection image", use_container_width=True)
